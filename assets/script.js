3d880cf8d1bfa693b77364dd42d26a08a63da59f
ConScorn <connorscorn@hotmail.com>
2021-08-11 21:36:47 -0500

    removed comments and console logs

--- assets/script.js
+++ assets/script.js
@@ -88,32 +88,21 @@ let upperCaseCharacters = [
   "Z",
 ];
 
-// This array contains all characters, in all 4 arrays
-// let allChars = [
-//   lowerCaseCharacters,
-//   upperCaseCharacters,
-//   numericCharacters,
-//   specialCharacters,
-// ];
-
 // This array will contain the types of characters the user has selected
 let possibleChars = [];
-
-// This array will contain one of each type of character included in the 'allChars' array
-let haveChars = [];
 
 // The user's input for password length will be pushed to this global variable
 let passwordLengthFinal = [];
 
 function randChar(array) {
   let index = Math.floor(Math.random() * array.length);
-  console.log(index);
   return array[index];
 }
 
 function generatePassword() {
   let password = [];
 
+  // prompts user for password length
   let passwordLength = window.prompt(
     "How long will your password be? Between 8-128"
   );
@@ -126,28 +115,26 @@ function generatePassword() {
     passwordLengthFinal.push(passwordLength);
   }
 
+  // prompts user for whether or not to include lower case letters
   function lowerCaseLetters() {
     let lowercase = window.confirm(
       "Will your password have lower case letters?"
     );
     if (lowercase === true) {
       possibleChars.push(...lowerCaseCharacters);
-      haveChars.push(randChar(lowerCaseCharacters));
-      console.log(haveChars);
       window.alert("Got it! Your password will include lower case letters.");
     } else {
       window.alert("Got it! No lower case letters will be included.");
     }
   }
 
+  // prompts user for whether or not to include upper case letters
   function upperCaseLetters() {
     let uppercase = window.confirm(
       "Will your password have upper case characters?"
     );
     if (uppercase === true) {
       possibleChars.push(...upperCaseCharacters);
-      haveChars.push(randChar(upperCaseCharacters));
-      console.log(haveChars);
       window.alert(
         "Sounds good! Your password will include upper case letters."
       );
@@ -156,24 +143,22 @@ function generatePassword() {
     }
   }
 
+  // prompts user for whether or not to include numbers
   function numericChars() {
     let number = window.confirm("Will your password have numbers?");
     if (number === true) {
       possibleChars.push(...numericCharacters);
-      haveChars.push(randChar(numericCharacters));
-      console.log(haveChars);
       window.alert("Alright! Your password will include numbers.");
     } else {
       window.alert("Alright! No numbers will be included.");
     }
   }
 
+  // prompts user for whether or not to include special characters
   function specialChars() {
     let special = window.confirm("Will your password have special characters?");
     if (special === true) {
       possibleChars.push(...specialCharacters);
-      haveChars.push(randChar(specialCharacters));
-      console.log(possibleChars);
       window.alert(
         "No Problem! Your password will include special characters."
       );
@@ -187,9 +172,9 @@ function generatePassword() {
   numericChars();
   specialChars();
 
+  // loops through the list of all possible characters based on user choice, and chooses one each time
   for (let i = 0; i < passwordLengthFinal; i++) {
     password.push(randChar(possibleChars));
-    console.log(possibleChars);
   }
   return password.join("");
 }
